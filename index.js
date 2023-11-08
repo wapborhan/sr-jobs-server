053b1e6fded818603d08930fe624b23cfc0b9211
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-08 19:57:01 +0600

    Deploy Vercel

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = 3300;
+const port = process.env.PORT || 3300;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -23,10 +28,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const dbJobs = client.db("jobsDB").collection("jobs");
     const dbapplied = client.db("jobsDB").collection("applied");
+
+    app.get("/", async (req, res) => {
+      console.log("Server Start");
+    });
 
     app.get("/jobs", async (req, res) => {
       const result = await dbJobs.find().toArray();
@@ -68,7 +77,18 @@ async function run() {
     });
 
     app.get("/applied", async (req, res) => {
-      const result = await dbapplied.find().toArray();
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await dbapplied.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/applied/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await dbapplied.findOne(query);
       res.send(result);
     });
 
@@ -105,10 +125,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-app.get("/", async (req, res) => {
-  console.log("Server Start");
-});
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
