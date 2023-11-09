998e7daf3da43e09eb62768569688160285bebc6
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-09 19:10:45 +0600

    JWT implement and verify token

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3300;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
+app.use(cookieparser());
+
+// MongoDB
 const uri = `mongodb+srv://srjobs:aoxOIWgdlHQjEE3g@cluster0.7dbji.mongodb.net/?retryWrites=true&w=majority`;
 // const uri = `mongodb+srv://${process.env.DB_NAMEUSER}:${process.env.DB_PASSCODE}@cluster0.7dbji.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,6 +30,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Not authorized " });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "authorized " });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +55,21 @@ async function run() {
 
     app.get("/", async (req, res) => {
       res.send("Server Start");
+    });
+
+    app.post("/auth", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      // console.log(token);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
     });
 
     app.get("/jobs", async (req, res) => {
@@ -72,7 +111,13 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/applied", async (req, res) => {
+    app.get("/applied", verifyToken, async (req, res) => {
+      //
+
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -94,7 +139,10 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/myjobs", async (req, res) => {
+    app.get("/myjobs", verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -108,6 +156,11 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await dbJobs.deleteOne(query);
       res.send(result);
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
     // Send a ping to confirm a successful connection
